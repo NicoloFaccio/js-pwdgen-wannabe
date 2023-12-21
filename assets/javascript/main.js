6000c145280a09a68b12d20dab644b11710da390
NicoloFaccio <nicolo.faccio1@gmail.com>
2023-12-21 15:53:15 +0100

    fix(Js): fixed the position of the elemts inside the document.writeln and the console.log

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -5,18 +5,9 @@ let userSurname= prompt ("Inserire il proprio cognome:" , "Rossi")
 
 let userFavoriteColor= prompt ("Inserire il proprio colore preferito:" , "Verde")
 
-let randInt = Math.floor(Math.random()*100);
+let randomNumber = Math.floor(Math.random()*100);
 
 //Section ABOUT THE DOCUMENTS AND THE CONSOLES  
-document.writeln("<span>" + userName + "</span>")
-console.log(userName)
+document.writeln("<p>" + userName + userSurname + userFavoriteColor + randomNumber + "</p>")
 
-document.writeln("<span>" + userSurname + "</span>")
-console.log(userSurname)
-
-document.writeln("<span>" + userFavoriteColor + "</span>")
-console.log(userFavoriteColor)
-
-document.writeln("<span>" + randInt + "</span>");
-console.log(randInt);
-
+console.log(userName + userSurname + userFavoriteColor + randomNumber)
