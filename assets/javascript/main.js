69583c3ed2e38c715b368a6e0ef0986cf6e05af1
NicoloFaccio <nicolo.faccio1@gmail.com>
2023-12-21 14:56:42 +0100

    feat(Js); created the 3 prompts

---
+++ assets/javascript/main.js
@@ -0,0 +1,12 @@
+//Section about the inictation of the variables
+let userName= prompt ("Inserire il proprio nome:" , "Fabio")
+document.writeln(userName)
+console.log(userName)
+
+let userSurname= prompt ("Inserire il proprio cognome:" , "Rossi")
+document.writeln(userSurname)
+console.log(userSurname)
+
+let userFavoriteColor= prompt ("Inserire il proprio colore preferito:" , "Verde")
+document.writeln(userFavoriteColor)
+console.log(userFavoriteColor)
