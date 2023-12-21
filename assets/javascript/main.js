6ea683545382ed3cd1da97d84e4dfa35467376b5
NicoloFaccio <nicolo.faccio1@gmail.com>
2023-12-21 15:41:54 +0100

    feat(Js): added the number

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,13 +1,22 @@
-//Section about the inictation of the variables
+//Section ABOUT THE INITATION OF THE VARIABLES
 let userName= prompt ("Inserire il proprio nome:" , "Fabio")
+
+let userSurname= prompt ("Inserire il proprio cognome:" , "Rossi")
+
+let userFavoriteColor= prompt ("Inserire il proprio colore preferito:" , "Verde")
+
+let randInt = Math.floor(Math.random()*100);
+
+//Section ABOUT THE DOCUMENTS AND THE CONSOLES  
 document.writeln("<span>" + userName + "</span>")
 console.log(userName)
 
-let userSurname= prompt ("Inserire il proprio cognome:" , "Rossi")
 document.writeln("<span>" + userSurname + "</span>")
 console.log(userSurname)
 
-let userFavoriteColor= prompt ("Inserire il proprio colore preferito:" , "Verde")
 document.writeln("<span>" + userFavoriteColor + "</span>")
 console.log(userFavoriteColor)
+
+document.writeln("<span>" + randInt + "</span>");
+console.log(randInt);
 
