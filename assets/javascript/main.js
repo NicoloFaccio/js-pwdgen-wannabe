99042128c501a7facb71e6f19a621068ff496ca3
NicoloFaccio <nicolo.faccio1@gmail.com>
2023-12-21 15:10:52 +0100

    feat(Js): used the document.writeln and the console.log. Also put the tag span inside the documents.writeln

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,12 +1,13 @@
 //Section about the inictation of the variables
 let userName= prompt ("Inserire il proprio nome:" , "Fabio")
-document.writeln(userName)
+document.writeln("<span>" + userName + "</span>")
 console.log(userName)
 
 let userSurname= prompt ("Inserire il proprio cognome:" , "Rossi")
-document.writeln(userSurname)
+document.writeln("<span>" + userSurname + "</span>")
 console.log(userSurname)
 
 let userFavoriteColor= prompt ("Inserire il proprio colore preferito:" , "Verde")
-document.writeln(userFavoriteColor)
+document.writeln("<span>" + userFavoriteColor + "</span>")
 console.log(userFavoriteColor)
+
